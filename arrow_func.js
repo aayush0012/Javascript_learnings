416906448a8f661d1f02ddc38a650cdf90736bc1
aayush0012 <bhattaayush2006@gmail.com>
2026-04-06 18:03:51 +0530

    learned arrow function in js

---
+++ arrow_func.js
@@ -0,0 +1,9 @@
+// Multiple methods to write a function Arrow method is one such method
+// Arrow functions are used in Mordern JS ES6
+// we mostly declare it with a const keyword not necesarry 
+const solve =(a,b)=>{
+    console.log("The sum is",a+b); 
+}
+let a=10 ;
+let b=20; 
+solve(a,b); 
